0c460a6dbb6762cef6a99afe67a0bee06b62a3f2
KazuakiTakahashi <ok220075@oks.iput.ac.jp>
2024-07-20 11:13:11 +0900

    'PostgreSQLによる商品データ、ユーザーの定義、レシピ検索の簡易実装'

--- development/Docker/api/api.py
+++ development/Docker/api/api.py
@@ -4,26 +4,24 @@ from pydantic import BaseModel
 
 from io import BytesIO # メモリ上でバイナリデータを扱うためのモジュール
 import matplotlib.pyplot as plt
-#import mysql.connector
-#import datetime
+import psycopg2
+import json
+
+# local import
+from components.Rakuten_recipe_api import req_recipe
 
 # データベース接続設定
-#config = {
-#  'user': 'root',     # ここにユーザー名を入れます
-#  'password': 'rootpass', # ここにパスワードを入れます
-#  'host': 'mysql-db',    # ここにホストを入れます（mysql-db）
-#  'database': 'test', # ここにデータベース名を入れます
-#  'raise_on_warnings': True
-#}
-
+connection = psycopg2.connect(
+    "host=db dbname=postgres user=postgres password=rootpass"
+    )
 #
 # データ形式の指定
 #
 class Item(BaseModel):
     name: str
-    id: int # DB内に保存されるID
+    id: str # DB内に保存されるID
     price: float
-    n: int
+    amount: int
     about: Optional[str] = None
 
 class Resipe(BaseModel):
@@ -59,12 +57,57 @@ async def search_cam(cap : UploadFile):
     # ここにカメラ画像検索処理を記述
     memory = await cap.read()
     image = plt.imread(BytesIO(memory), format = cap.filename.split('.')[-1]) # image ･･･ 画像インスタンス。これをモデルの入力として使えるか要検証
-    return {"item_list":[{"name":"item1","id":1,"price":1000,"amount":1,"about":"item1"},{"name":"item2","id":2,"price":2000,"amount":1,"about":"item2"}]}
+
+    # 
+    # ここに画像検索処理を記述
+    #
+    results = ["オレンジ","バナナ"] # 仮の結果
+
+    # 検索結果を返す
+    res = {"item_list":[]}
+    for result in results:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM items WHERE name = %s", (result,))
+        item = cursor.fetchone()
+        cursor.close()
+        item = Item(id=item[0], name=item[1], price=item[2], about=item[3], amount=1)
+        res["item_list"].append(item.dict())
+
+    #return {"item_list":[{"name":"item1","id":1,"price":1000,"amount":1,"about":"item1"},{"name":"item2","id":2,"price":2000,"amount":1,"about":"item2"}]}
+    return res
 
 @app.post("/resipe/search")
-async def search_resipe(resipe: Item):
+async def search_resipe(Material: Item,
+                        cols = ['recipeTitle','recipeMaterial'],
+                        keys = ['name','items']):
+    r = req_recipe('1031564129861406174')
+
+    res = r(Material.name) # レシピ検索 : 第一引数に素材を指定
+
+    Response = []
+    for i in range(len(res)):
+        Response.append({key: res[i][col] for col, key in zip(cols,keys)})
+
+    
+    for i, res in enumerate(Response):
+        items = []
+        for item in res['items']:
+            cursor = connection.cursor()
+            cursor.execute("SELECT * FROM items WHERE name = %s", (item,))
+            item_detail = cursor.fetchone()
+            cursor.close()
+            if item_detail is None:
+                # 商品が見つからなかった場合
+                item = Item(id="0", name=item, price=0, about="not found", amount=1)
+            else:
+                # 商品が見つかった場合
+                item = Item(id=item_detail[0], name=item_detail[1], price=item_detail[2], about=item_detail[3], amount=1)
+            items.append(item)
+        Response[i]['items'] = items # DBに基づく商品情報に変換
+    
     # ここにレシピ検索処理を記述
-    return {"resipe_list":[{"name":"resipe1","items":[{"name":"item1","id":1,"price":1000,"amount":1,"about":"item1"},{"name":"item2","id":2,"price":2000,"amount":1,"about":"item2"}]}]}
+    return {"resipe_list":Response}
+    #return {"resipe_list":[{"name":"resipe1","items":[{"name":"item1","id":1,"price":1000,"amount":1,"about":"item1"},{"name":"item2","id":2,"price":2000,"amount":1,"about":"item2"}]}]}
 
 @app.post("/cart/sum")
 async def sum_cart(cart: cart):
---
+++ development/Docker/api/components/Rakuten_recipe_api.py
@@ -0,0 +1,44 @@
+import requests
+import time
+import json
+from pprint import pprint
+import pandas as pd
+
+class req_recipe:
+  def __init__(self, application_id):
+    self.application_id = application_id
+    self.category_list_url = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?format=json'
+    self.category_ranking_url = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?format=json'
+  
+  def __call__(self, search_word):
+    params = {
+        "format" : "json",
+        "applicationId" : self.application_id
+    }
+    res = requests.get(self.category_list_url, params=params)
+    data = res.json()
+    df = pd.DataFrame(data['result']['small'])
+    category_list = df['categoryName'].to_list()
+
+    try:
+      df_keyword = df.query(f'categoryName.str.contains("{search_word}")', engine='python')
+    except:
+      return None
+    
+    result = []
+    for search_idx, [index, row] in enumerate(df_keyword.iterrows()):
+      time.sleep(3)
+      params = {
+          "format" : "json",
+          "applicationId" : self.application_id,
+          "categoryId" : row['categoryUrl'].split('/')[4] # f"{large}-{medium}-{small}" の形式
+      }
+      res = requests.get(self.category_ranking_url, params=params)
+
+      for i in range(len(res.json()['result'])):
+        result.append(dict(res.json()['result'][i]))
+    return result
+
+if __name__ == '__main__':
+  r = req_recipe('1031564129861406174')
+  pprint(r('バナナ'))
